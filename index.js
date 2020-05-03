59d5126ac90ef6d7b82d4db83f2f3eb35b1004c6
Carmen Dukes <carmendukes@gmail.com>
2020-05-02 19:09:33 -0700

    update output of images

--- index.js
+++ index.js
@@ -66,13 +66,5 @@
 
  
 
-    /*
-  function getDogImage() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert('Something went wrong. Try again later.'));
-  }
-  */
+   
  
