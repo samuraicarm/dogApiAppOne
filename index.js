83f6dd56ea18f5dcf323fd1e1d70cd3d1e009559
Carmen Dukes <carmendukes@gmail.com>
2020-05-06 18:46:45 -0700

    updated files

--- index.js
+++ index.js
@@ -1,67 +1,72 @@
-  'use strict';
-  let dogLink = " ";
+'use strict';
 
-  $(document).ready(function(){
-    console.log("ready");
+let dogLink = " ";
+let dogApi ="https://dog.ceo/api/breeds/image/random/";
+
+$(document).ready(function(){
+  console.log("ready");
+});
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+   let userInput = getValue();
+    appendURL(userInput);
+    getDogImage(); 
   });
 
-  $(function() {
-    console.log('App loaded! Waiting for submit!');
-    watchForm();
-  });
+}
 
-  function watchForm() {
-    $('form').submit(event => {
-      event.preventDefault();
-     let userInput = getBreed();
-      updateURL(userInput);
-      getDogImage(); 
-    });
+//get value from form
+function getValue(){
+  let formValue = document.getElementById('myInput').value;
+  return formValue 
+}
+
+
+//append URL to include form value
+function appendURL(formValue){
+  dogLink = dogApi.concat(formValue);
+  console.log(formValue) 
+  
+} 
+
+
+//fetch dog image from api
+
+function getDogImage() {
+  fetch(dogLink)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  for (var i = 0; responseJson.message.length > i; i++)
+  {
+    let img = new Image(200, 200);
+    img.src = responseJson.message[i];
+
+    let src = document.getElementById("dogImages");
+    src.appendChild(img);
 
   }
-
-  //get value from form
-  function getBreed(){
-    let dogBreed = document.getElementById('userInput').value;
-    return dogBreed
-  }
+  //display the results section
+  $('.results').removeClass('hidden');
+}
 
 
-  //update URL to include form value
-  function updateURL(dogBreed){
-   dogLink = `https://dog.ceo/api/breed/${dogBreed}/images/random/1`;
-    console.log(dogLink) 
-  } 
 
-  //fetch dog image from api
+ 
 
-  function getDogImage() {
-    fetch(dogLink)
-      .then(response => response.json())
-      .then(responseJson => displayResults(responseJson))
-      .catch(error => alert('Breed not found. Try again.'));
-  }
-
-
-  function displayResults(responseJson) {
-    console.log(responseJson);
-    //replace the existing image with the new one
-    $('.results-img').replaceWith(
-      `<img src="${responseJson.message}" class="results-img" width="500" height="500">`)
-
-    //display the results section
-    $('.results').removeClass('hidden');
-  }
-
-  //if no results found, post default image
-   function displayError (responseJson){
-     if (responseJson.message.status = 'error') {
-     $('.results-img').replaceWith(
-      `<img src="/img/sadpuppy.jpg" class="results-img" width="500" height="500">`)
-      $('.results').removeClass('hidden');
-      $('.text').removeClass('hidden');
-     }
-    }
    
    
  
